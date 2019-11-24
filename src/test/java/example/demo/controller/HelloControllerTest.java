2a3fc02049d2949cdb04ce02cc31a0c372626ec8
takumi34 <1shiranui@gmail.com>
2019-11-24 23:45:32 +0900

    add HelloTestController

---
+++ src/test/java/example/demo/controller/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package example.demo.controller;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest
+class HelloControllerTest {
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
+    @Test
+
+    public void testHello() {
+        HttpRequest<String> request = HttpRequest.GET("/hello");
+        String body = client.toBlocking().retrieve(request);
+
+        assertNotNull(body);
+        assertEquals("Hell Chika", body);
+    }
+}
